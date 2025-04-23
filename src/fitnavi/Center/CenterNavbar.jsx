8cacdd7cf6ab9fb738df0c142a4e7fb9a0a5f93b
jodi0813 <jodi08131111@gmail.com>
2025-04-23 16:23:27 +0800

    更新找場地的場館資料&環境設施

--- src/fitnavi/Center/CenterNavbar.jsx
+++ src/fitnavi/Center/CenterNavbar.jsx
@@ -3,6 +3,7 @@ import "./CenterNavbar.scss";
 function CenterNavbar() {
     return (
         <>
+            {/* 中間的選單列 */}
             <div class="centerMenu">
                 <ul >
                     <li>
@@ -16,11 +17,18 @@ function CenterNavbar() {
                     </li>
                 </ul>
             </div>
+            {/* 一、場館資料 */}
+            <section id="centerInformation">
+                {/* 基本資料 */}
+                <div class="basicVenueInfo">
+                    <div class="informationLeft">
+                        {/* 標題 */}
+                        <div class="centermenuTitle">
+                            <h2 >基本資料</h2>
+                            <span class="subtitle">Basic Venue Info</span>
+                        </div>
 
-            <section class="centerInformation">
-                <div class="information">
-                    <div>
-                        <h2 class="">基本資料</h2>
+                        <hr class="divider"></hr>
                         <ul>
                             <li>
                                 <strong>地址</strong><br />
@@ -40,28 +48,38 @@ function CenterNavbar() {
                             </li>
                         </ul>
                     </div>
-                    <div>
+                    <div class="informationRight">
                         <img src="./images/map.png" alt="場館地圖位置" />
                     </div>
                 </div>
-
-                <div class="outward">
-                    <div>
+                {/* 場景外觀 */}
+                <div class="venueExterior">
+                    {/* 標題 */}
+                    <div class="centermenuTitle">
                         <h2>場館外觀</h2>
-
-                        <div class="centerPhotos">
-                            <button>‹</button>
-                            <img src="/images/center01.jpg" alt="外觀照片1" />
-                            <img src="/images/center02.jpg" alt="外觀照片2" />
-                            <img src="/images/center03.jpg" alt="外觀照片3" />
-                            <button>›</button>
-                        </div>
-
+                        <span class="subtitle"> Venue Exterior</span>
+                    </div>
+                    <hr class="divider"></hr>
+                    {/* 場景外觀的照片 */}
+                    <div class="centerPhotos">
+                        <button>‹</button>
+                        <img src="/images/center01.jpg" alt="外觀照片1" />
+                        <img src="/images/center02.jpg" alt="外觀照片2" />
+                        <img src="/images/center03.jpg" alt="外觀照片3" />
+                        <button>›</button>
                     </div>
                 </div>
+                {/* 入場費用+聯絡方式 */}
                 <div class="costContact">
-                    <div>
-                        <h2>入場費用</h2>
+                    {/* 入場費用 */}
+                    <div class="entranceFee">
+                        {/* 標題 */}
+                        <div class="centermenuTitle">
+                            <h2>入場費用</h2>
+                            <span class="subtitle">Entrance Fee</span>
+                        </div>
+                        <hr class="divider"></hr>
+
                         <ul>
                             <li>
                                 <strong>單次入場</strong><br />
@@ -75,9 +93,13 @@ function CenterNavbar() {
                             </li>
                         </ul>
                     </div>
-
-                    <div>
-                        <h2>聯絡方式</h2>
+                    {/* 聯絡方式 */}
+                    <div class="contactInfo">
+                        <div class="centermenuTitle">
+                            <h2>聯絡方式</h2>
+                            <span class="subtitle">Contact Info.</span>
+                        </div>
+                        <hr class="divider"></hr>
                         <ul>
                             <li>
                                 <strong>連絡電話</strong><br />
@@ -89,6 +111,69 @@ function CenterNavbar() {
                             </li>
                         </ul>
                     </div>
+                </div>
+            </section>
+
+            {/* 二、環境設施 */}
+            <section id="facilities">
+                {/* 有氧器材 */}
+                <div class="cardioEquipmentArea">
+                    <div class="centermenuTitle">
+                        <h2>有氧器材</h2>
+                        <span class="subtitle">Cardio Equipment Area</span>
+                    </div>
+                    <hr class="divider"></hr>
+
+                    {/* 有氧器材的照片 */}
+                    <div class="cardioEquipmentAreaPhotos">
+                        <button>‹</button>
+                        <img src="/images/cardioEquipment01.jpg" alt="有氧器材照片1" />
+                        <img src="/images/cardioEquipment02.jpg" alt="有氧器材照片2" />
+                        <img src="/images/cardioEquipment03.jpg" alt="有氧器材照片3" />
+                        <button>›</button>
+                    </div>
+                    <div>設備器材</div>
+                    <hr />
+                    <div>跑步機 飛輪機 滑步機 登階機 划船機 戰繩設備 壺鈴</div>
+                </div>
+                {/* 重訓器材(X) */}
+                <div class="weightTrainingArea">
+                    <div class="centermenuTitle">
+                        <h2>重訓器材</h2>
+                        <span class="subtitle">Weight Training Area</span>
+                    </div>
+                    <hr class="divider"></hr>
+
+                    {/* 重訓器材的照片 */}
+                    <div class="weightTrainingAreaPhotos">
+                        <button>‹</button>
+                        <img src="/images/weightTraining01.jpg" alt="重訓器材照片1" />
+                        <img src="/images/weightTraining02.jpg" alt="重訓器材照片2" />
+                        <img src="/images/weightTraining03.jpg" alt="重訓器材照片3" />
+                        <button>›</button>
+                    </div>
+                    <div>設備器材</div>
+                    <hr />
+                    <div>跑步機 飛輪機 滑步機 登階機 划船機 戰繩設備 壺鈴</div>
+                </div>
+                {/* 更衣淋浴(X) */}
+                <div class="showerArea">
+                    <div class="centermenuTitle">
+                        <h2>更衣淋浴</h2>
+                        <span class="subtitle">Changing & Shower Area</span>
+                    </div>
+                    <hr class="divider"></hr>
+
+                    {/* 更衣淋浴的照片 */}
+                    <div class="showerAreaPhotos">
+                        <button>‹</button>
+                        <img src="/images/showerArea01.jpg" alt="更衣淋浴照片1" />
+                        <img src="/images/showerArea02.jpg" alt="更衣淋浴照片2" />
+                        <button>›</button>
+                    </div>
+                    <div>設備器材</div>
+                    <hr />
+                    <div>跑步機 飛輪機 滑步機 登階機 划船機 戰繩設備 壺鈴</div>
                 </div>
             </section>
 
