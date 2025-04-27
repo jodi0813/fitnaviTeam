a3bf6bb83d63e197d4f74fd885774aad8f45f666
Jodi <jodi08131111@gmail.com>
2025-04-27 12:32:20 +0800

    調整健身教練字體

--- src/fitnavi/Coach/Coach.jsx
+++ src/fitnavi/Coach/Coach.jsx
@@ -3,65 +3,66 @@ import "./Coach.scss";
 function Coach() {
   return (
     <>
-    <div id="coachMain">
-      <header className="coachTitle">
-        <h2>健身教練</h2>
-        <p>PERSONAL TRAINER</p>
-      </header>
-
-      <form
-        action=""
-        method="post"
-        name="personal-search-form"
-        id="personal-search-form"
-        title="健身教練搜尋"
-      >
-        <label for="area"></label>
-        <select name="area" id="area">
-          <option value="">地區</option>
-          <option value="台北市">台北市</option>
-          <option value="新北市">新北市</option>
-        </select>
-        <label for="trainingNeeds"></label>
-        <select name="trainingNeeds" id="trainingNeeds">
-          <option value="">訓練需求</option>
-          <option value=""></option>
-          <option value=""></option>
-        </select>
-        <label for="sex"></label>
-        <select name="sex" id="sex">
-          <option value="性別">性別</option>
-          <option value="男">男</option>
-          <option value="女">女</option>
-        </select>
-        <label for="keyword-search"></label>
-        <input
-          type="search"
-          name="keyword-search"
-          id="keyword-search"
-          placeholder="關鍵字搜尋"
-        ></input>
-        <input type="submit" value="搜尋" className="submit"></input>
-      </form>
-      <div className="coach">
-        <div className="all-coach">全部教練</div>
-        {/* 教練小卡 */}
-        <div className="coach-card">
-          <div className="image-container">
-            <img src="./images/trainer.png" alt="教練照片" />
-            <span className="label">教練</span>
-          </div>
-          <div className="coach-name">名字</div>
-          <div className="hashtag">
-            <a href="#">#專業</a>
-            <a href="#">#專業</a>
-            <a href="#">#專業</a>
-          </div>
-          <p className="coach-details">
-            這是教練或營養師的簡介內容，這是教練或營養師的簡介內容，這是教練或營養師的簡介內容。
-          </p>
+      <div id="coachMain">
+        <header className="coachTitle">
+          <h2>健身教練</h2>
+          <p>PERSONAL TRAINER</p>
+        </header>
+        <div className="searchBox">
+          <form
+            action=""
+            method="post"
+            name="personal-search-form"
+            id="personal-search-form"
+            title="健身教練搜尋"
+          >
+            <label for="area"></label>
+            <select name="area" id="area">
+              <option value="">地區</option>
+              <option value="台北市">台北市</option>
+              <option value="新北市">新北市</option>
+            </select>
+            <label for="trainingNeeds"></label>
+            <select name="trainingNeeds" id="trainingNeeds">
+              <option value="">訓練需求</option>
+              <option value=""></option>
+              <option value=""></option>
+            </select>
+            <label for="sex"></label>
+            <select name="sex" id="sex">
+              <option value="性別">性別</option>
+              <option value="男">男</option>
+              <option value="女">女</option>
+            </select>
+            <label for="keyword-search"></label>
+            <input
+              type="search"
+              name="keyword-search"
+              id="keyword-search"
+              placeholder="關鍵字搜尋"
+            ></input>
+            <input type="submit" value="搜尋" className="submit"></input>
+          </form>
         </div>
-      </div>
+        <div className="coach">
+          <div className="all-coach">全部教練</div>
+          {/* 教練小卡 */}
+          <div className="coach-card">
+            <div className="image-container">
+              <img src="./images/trainer.png" alt="教練照片" />
+              <span className="label">教練</span>
+            </div>
+            <div className="coach-name">名字</div>
+            <div className="hashtag">
+              <a href="#">#專業</a>
+              <a href="#">#專業</a>
+              <a href="#">#專業</a>
+            </div>
+            <p className="coach-details">
+              這是教練或營養師的簡介內容，這是教練或營養師的簡介內容，這是教練或營養師的簡介內容。
+            </p>
+          </div>
+        </div>
       </div>
     </>
   );
