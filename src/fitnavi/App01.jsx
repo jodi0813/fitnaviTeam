1a94bcab393e5c3b6d0fde76518d7274808d9cc2
amber022834 <sodagreen0228@gmail.com>
2025-05-02 11:50:08 +0800

    更新footer(樣式先用css寫)

--- src/fitnavi/App01.jsx
+++ src/fitnavi/App01.jsx
@@ -5,6 +5,7 @@ import CenterMap from "./Center/CenterMap";
 import Article from "./Article/Article";
 import Center from "./Center/Center";
 import CoachIntro from "./CoachIntro/Coachintro";
+import Footer from "./Footer/Footer";
 
 function App() {
   return (
@@ -13,11 +14,14 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/coach" element={<Coach />}></Route>
+          <Route path="/coach/:id" element={<Coach />}></Route>
           <Route path="/centerMap" element={<CenterMap />}></Route>
           <Route path="/article" element={<Article />}></Route>
           <Route path="/center" element={<Center />}></Route>
           <Route path="/coachintro" element={<CoachIntro />}></Route>
         </Routes>
+        <Footer/>
+        
       </div>
     </>
   );
