619551dc902f4ef5153a7fbaf91e5a0858e78c25
amber022834 <sodagreen0228@gmail.com>
2025-05-03 20:45:40 +0800

    更新文章總攬頁

--- src/components/Articlecards.jsx
+++ src/components/Articlecards.jsx
@@ -1,9 +1,14 @@
-function Articlecards({ date, img, title, content, tags, category, className }){
+import React from "react";
+// import { Link } from "react-router-dom";
+
+function Articlecards({ date, img, title, content, tags, category, className }) {
     return (
         <>
+            {/* <Link to={`/allarticle/${id}`} className={`article-card ${className}`}></Link> */}
             <div className={className}>
                 <p>{date}</p>
-                <div className="img-box"><img src={img} alt="" /></div>
+                <div className="img-box">
+                    <img src={img} alt="" /></div>
                 <div className="content-box">
                     {/* 因為帶有html標籤所以要輸入dangerouslySetInnerHtml */}
                     <h4 dangerouslySetInnerHTML={{ __html: title }} />
--- src/fitnavi/App01.jsx
+++ src/fitnavi/App01.jsx
@@ -3,11 +3,12 @@ import Navbar from "./Navbar/Navbar";
 import Coach from "./Coach/Coach";
 import CenterMap from "./Center/CenterMap";
 import Article from "./Article/Article";
+import Allarticle from "./Allarticle/Allarticle";
 import Center from "./Center/Center";
 import CoachIntro from "./CoachIntro/Coachintro";
 import Footer from "./Footer/Footer";
 import MemberList from "./MemberList/MemberList";
-import Allarticle from "./Allarticle/Allarticle";
+
 
 function App() {
   return (
@@ -16,9 +17,10 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/coach" element={<Coach />}></Route>
-          <Route path="/coach/:id" element={<Coach />}></Route>
+          
           <Route path="/centerMap" element={<CenterMap />}></Route>
-          <Route path="/article" element={<Article />}></Route>
+          <Route path="/allarticle" element={<Allarticle />} />  {/* 文章列表頁 */}
+          {/* <Route path="/allarticle/:card1" element={<Article />} />  */}
           <Route path="/center" element={<Center />}></Route>
           <Route path="/coachintro" element={<CoachIntro />}></Route>
           <Route path="/memberlist" element={<MemberList />}></Route>
