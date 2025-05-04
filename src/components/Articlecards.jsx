6686f4e9ec62b113a6266eb9381c549ce620e636
amber022834 <sodagreen0228@gmail.com>
2025-05-04 21:00:41 +0800

    更新articlecard的css

--- src/components/Articlecards.jsx
+++ src/components/Articlecards.jsx
@@ -1,26 +1,27 @@
 import React from "react";
 // import { Link } from "react-router-dom";
+import styles from "./Articlecards.module.css";
 
 function Articlecards({ date, img, title, content, tags, category, className }) {
     return (
         <>
             {/* <Link to={`/allarticle/${id}`} className={`article-card ${className}`}></Link> */}
-            <div className={className}>
-                <p>{date}</p>
-                <div className="img-box">
+            <div className={`${styles.card} ${className}`}>
+                <p className={styles.data}>{date}</p>
+                <div className={styles.imgbox}>
                     <img src={img} alt="" /></div>
-                <div className="content-box">
+                <div className={styles.contentBox}>
                     {/* 因為帶有html標籤所以要輸入dangerouslySetInnerHtml */}
                     <h4 dangerouslySetInnerHTML={{ __html: title }} />
                     <p>{content}</p>
-                    <div className="tag">
+                    <div className={styles.tag}>
                         {tags.map((tag, index) => {
                             const tagText = `#${tag}`;  // 先將 tag 字串加上 `#` 符號
-                            return <p key={index}>{tagText}</p>; // 返回包含 tagText 的 <p> 標籤
+                            return <p key={index} className={styles.tagText}>{tagText}</p>;// 返回包含 tagText 的 <p> 標籤
                         })}
                     </div>
                 </div>
-                <div className="tag-box">{category}</div>
+                <div className={styles.tagBox}>{category}</div>
             </div>
         </>
     )
