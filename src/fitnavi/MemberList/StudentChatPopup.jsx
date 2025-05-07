65149f003d374b9a749972dd27c885c503fecdce
jodi0813 <jodi08131111@gmail.com>
2025-05-07 14:02:19 +0800

    後台管理留言功能

--- src/fitnavi/MemberList/StudentChatPopup.jsx
+++ src/fitnavi/MemberList/StudentChatPopup.jsx
@@ -2,17 +2,17 @@ import "./StudentChatPopup.scss";
 function StudentChatPopup({ onClose }) {
     return (
         <>
-            <section class="chat-popup">
+            <section id="chat-popup">
                 <header class="chat-header">
                     <h2>聯絡講師</h2>
                     <div class="teacher-info">
-                        <span>飛飛 Feifei</span>
+                        <span>飛飛 Feifei</span><br />
                         <span>feifei0303@gmail.com</span>
                     </div>
                     <span>訂單編號: #00596</span>
                     <button className="close-btn" onClick={onClose}>✕</button>
                 </header>
-
+               
                 <div class="messages">
                     {/* 用戶 */}
                     <div class="message user">
@@ -23,30 +23,30 @@ function StudentChatPopup({ onClose }) {
                     {/* 教練*/}
                     <div class="message coach">
                         <img src="./images/coachavatar.jpg" alt="教練頭像" class="avatar" />
-                        <div class="bubble">沒問題，<br/>那麼你第一堂課比較想針對哪一個部位進行訓練呢？</div>
+                        <div class="bubble">沒問題，<br />那麼你第一堂課比較想針對哪一個部位進行訓練呢？</div>
                     </div>
 
                     {/* 用戶 */}
                     <div class="message user">
-                        <img src="" alt="用戶頭像" class="avatar" />
+                        <img src="/images/useravatar.jpg" alt="用戶頭像" class="avatar" />
                         <div class="bubble">我想練試看看臀腿訓練！</div>
                     </div>
 
                     {/* 教練*/}
                     <div class="message coach">
-                        <img src="" alt="教練頭像" class="avatar" />
+                        <img src="./images/coachavatar.jpg" alt="教練頭像" class="avatar" />
                         <div class="bubble">好，臀腿訓練會非常累喔！前一天記得睡飽再來。</div>
                     </div>
 
                     {/* 用戶 */}
                     <div class="message user">
-                        <img src="" alt="用戶頭像" class="avatar" />
+                        <img src="/images/useravatar.jpg" alt="用戶頭像" class="avatar" />
                         <div class="bubble">沒問題，謝謝教練，明天19:00健身房見喔。</div>
                     </div>
 
-                   {/* 教練*/}
+                    {/* 教練*/}
                     <div class="message coach">
-                        <img src="" alt="教練頭像" class="avatar" />
+                        <img src="./images/coachavatar.jpg" alt="教練頭像" class="avatar" />
                         <div class="bubble">好的，明天見!!</div>
                     </div>
                 </div>
