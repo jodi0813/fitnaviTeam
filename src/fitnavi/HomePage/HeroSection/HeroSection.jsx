f42119d2a299ba2386043c2f9ff3509575ae4d42
Jodi <jodi08131111@gmail.com>
2025-05-09 14:07:39 +0800

    首頁

--- src/fitnavi/HomePage/HeroSection/HeroSection.jsx
+++ src/fitnavi/HomePage/HeroSection/HeroSection.jsx
@@ -2,7 +2,7 @@ import "./HeroSection.scss"
 function HeroSection() {
     return(
         <>
-        首頁影片區(可刪)
+        首頁影片區(繼旻可刪)
         </>
     )
 }export default HeroSection;
--- src/fitnavi/HomePage/NutritionistSection/NutritionistSection.jsx
+++ src/fitnavi/HomePage/NutritionistSection/NutritionistSection.jsx
@@ -2,7 +2,7 @@ import "./NutritionistSection.scss"
 function NutritionistSection() {
     return(
         <>
-        首頁營養師(可刪)
+        首頁營養師(芝瑩可刪)
         </>
     )
 }export default NutritionistSection;
