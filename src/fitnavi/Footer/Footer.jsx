d15efa201f21a91cc8cda98886e38c0069b74f06
amber022834 <sodagreen0228@gmail.com>
2025-05-13 00:31:53 +0800

    back to top效果

--- src/fitnavi/Footer/Footer.jsx
+++ src/fitnavi/Footer/Footer.jsx
@@ -9,17 +9,19 @@ function Footer() {
         <>
             <section id="footer">
 
-                <button className="Back-to-top-button" onClick=
-                    {() => {
-                        navigate('/HomePage');
-                        setTimeout(() => {
-                            window.scrollTo(0, 0);
-                        }, 100);
-
+                {/* 按按鈕後平順回到最上面 */}
+                <button
+                    className="Back-to-top-button"
+                    onClick={() => {
+                        window.scrollTo({
+                            top: 0,
+                            behavior: 'smooth',
+                        });
                     }}
-                    style={{ cursor: 'pointer' }} >
-                    <p style={{ cursor: 'pointer' }}>回頂部</p>
-                    <img src="/images/back-to-top.gif" alt="" style={{ cursor: 'pointer' }} />
+                    style={{ cursor: 'pointer' }}
+                >
+                    <p>回頂部</p>
+                    <img src="/images/back-to-top.gif" alt="" />
                 </button>
 
 
@@ -30,46 +32,54 @@ function Footer() {
                     <div className="footer-overlaytext">
                         <div className="footer-columntext">
                             <div className="explore">
-                                <h4>探索</h4>
+                                <h4>網站導覽</h4>
                                 <p onClick={() => {
                                     navigate('/Coach');
                                     window.scrollTo(0, 0);
-                                }} style={{ cursor: "pointer" }}>課程搜尋</p>
+                                }} style={{ cursor: "pointer" }}>健身教練</p>
+
                                 <p onClick={() => {
-                                    navigate('/CenterMap');
+                                    navigate('/Nutritionist');
                                     window.scrollTo(0, 0);
-                                }} style={{ cursor: "pointer" }}>場地介紹</p>
+                                }} style={{ cursor: "pointer" }}>營養師</p>
+
+                                <p onClick={() => {
+                                    navigate('/MyFavorite');
+                                    window.scrollTo(0, 0);
+                                }} style={{ cursor: "pointer" }}>我的收藏</p>
+
                                 <p onClick={() => {
                                     navigate('/Allarticle');
                                     window.scrollTo(0, 0);
-                                }} style={{ cursor: "pointer" }}>知識分享</p>
+                                }} style={{ cursor: "pointer" }}>知識文章</p>
+
                                 <p onClick={() => {
-                                    navigate('/Joinus');
+                                    navigate('/CenterMap');
                                     window.scrollTo(0, 0);
-                                }} style={{ cursor: "pointer" }}>合作夥伴</p>
+                                }} style={{ cursor: "pointer" }}>健身場地</p>
                             </div>
 
-                            <div className="partner">
+                            <div className="Joinus">
                                 <h4>合作夥伴</h4>
                                 <p onClick={() => {
                                     navigate('/MemberLogin');
                                     window.scrollTo(0, 0);
-                                }} style={{ cursor: "pointer" }}>登入後台</p>
-                                <p>合作夥伴支援中心</p>
-                                <p>將您的課程上線</p>
+                                }} style={{ cursor: "pointer" }}>加入我們</p>
+                                <p>會員管理</p>
+                                <p>問題反映</p>
                             </div>
 
                             <div className="aboutus">
                                 <h4>關於我們</h4>
                                 <p>我們如何運作</p>
-                                <p>媒體中心</p>
-                                <p>其他問題</p>
+                                <p>聯絡我們</p>
+                                <p>常見問題</p>
                             </div>
 
                             <div className="law">
                                 <h4>條款與條件</h4>
-                                <p>隱私權和cookies</p>
-                                <p>合作夥伴爭議解決中心</p>
+                                <p>隱私政策</p>
+                                <p>服務條款</p>
                             </div>
                         </div>
 
