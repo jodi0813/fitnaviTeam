6b1c4c11bd54529b65838e5127608b3cc7da76e1
jodi0813 <jodi08131111@gmail.com>
2025-05-16 16:09:12 +0800

    調整

--- src/fitnavi/Center/CenterMap.jsx
+++ src/fitnavi/Center/CenterMap.jsx
@@ -1,4 +1,4 @@
-import { Link, useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import { useState, useEffect } from "react";
 import "./CenterMap.scss";
 import MainTitle from "../../components/Title/MainTitle";
