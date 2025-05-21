d074291752325d692e8879057f93f1ef4569e96a
Jodi <jodi08131111@gmail.com>
2025-05-22 00:32:30 +0800

    調整畫面寬度

--- src/fitnavi/Navbar/Navbar.jsx
+++ src/fitnavi/Navbar/Navbar.jsx
@@ -1,7 +1,6 @@
 import { Link } from "react-router-dom";
 import "./Navbar.scss";
-import { useNavigate } from 'react-router-dom';
-
+import { useNavigate } from "react-router-dom";
 
 function Navbar() {
   const navigate = useNavigate();
@@ -19,31 +18,44 @@ function Navbar() {
               <span className="bar"></span>
               <span className="bar"></span>
             </button>
+            <div>
             <ul className="menu">
-
-              <li>
-                <Link to="/coach">健身教練</Link>
-              </li>
-              <li>
-                <Link to="/nutritionist">營養師</Link>
-              </li>
-              <li>
-                <Link to="/myfavorite">我的收藏</Link>
-              </li>
-              <li>
-                <Link to="/allarticle">知識文章</Link>
-              </li>
-              <li>
-                <Link to="/centerMap">健身場地</Link>
-              </li>
-              <li>
-                <Link to="./Joinus">加入我們</Link>
-              </li>
-              {/* <li>
-                <Link to="/memberlist">後台(暫放)</Link>
-              </li> */}
-            </ul>
-            <button type="button" className="nbr-login" onClick={() => navigate('MemberLogin')}>
+              <div>
+                <li>
+                  <Link to="/coach">健身教練</Link>
+                </li>
+              </div>
+              <div>
+                <li>
+                  <Link to="/nutritionist">營養師</Link>
+                </li>
+              </div>
+              <div>
+                <li>
+                  <Link to="/myfavorite">我的收藏</Link>
+                </li>
+              </div>
+              <div>
+                <li>
+                  <Link to="/allarticle">知識文章</Link>
+                </li>
+              </div>
+              <div>
+                <li>
+                  <Link to="/centerMap">健身場地</Link>
+                </li>
+              </div>
+              <div>
+                <li>
+                  <Link to="./Joinus">加入我們</Link>
+                </li>
+              </div>
+            </ul></div>
+            <button
+              type="button"
+              className="nbr-login"
+              onClick={() => navigate("MemberLogin")}
+            >
               登入 / 註冊
             </button>
           </nav>
