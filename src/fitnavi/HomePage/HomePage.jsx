75a18e95dd635b802bbe776d9b6484b015f710e9
weiichen2001 <weiichen2001@gmail.com>
2025-05-27 01:04:46 +0800

    首頁影片漸層

--- src/fitnavi/HomePage/HomePage.jsx
+++ src/fitnavi/HomePage/HomePage.jsx
@@ -1,35 +1,3 @@
-// import ArticleSection from "./ArticleSection/ArticleSection";
-// import HeroSection from "./HeroSection/HeroSection";
-// import NutritionistSection from "./NutritionistSection/NutritionistSection";
-// import SearchSection from "./SearchSection/SearchSection";
-// import TrainerSection from "./TrainerSection/TrainerSection";
-// import "./HomePage.scss";
-// function HomePage() {
-//   return (
-//     <>
-//       <main id="homePage">
-//         <section>
-//           <HeroSection />
-//         </section>
-
-//         <div className="view">
-//           <section className="homeSearchSection">
-//             <SearchSection />
-
-//             <TrainerSection />
-
-//             <NutritionistSection />
-
-//             <ArticleSection />
-//           </section>
-//         </div>
-
-
-//       </main>
-//     </>
-//   );
-// }
-// export default HomePage;
 import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import ArticleSection from "./ArticleSection/ArticleSection";
