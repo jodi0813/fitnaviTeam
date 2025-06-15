0e686eae085fd48693de4c1aa1091bac2a302317
Jodi <jodi08131111@gmail.com>
2025-06-15 14:39:17 +0800

    調整麵包屑標題

--- src/components/Title/MaintitleBread.jsx
+++ src/components/Title/MaintitleBread.jsx
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import "./MainTitleBread.scss";
 import { HiChevronRight } from "react-icons/hi";
+import { FaAngleLeft } from "react-icons/fa";
 function MainTitleBread({ title1, title2, breadcrumbList = [] }) {
   const back = useNavigate();
   return (
@@ -8,6 +9,9 @@ function MainTitleBread({ title1, title2, breadcrumbList = [] }) {
       <div className="mainTitleBreadBox">
         <header className="mainTitleBread">
           <div className="titleBoxBread">
+            <button onClick={() => back(-1)} className="titleBackBt">
+              <FaAngleLeft size={20} />
+            </button>
             <h2 className="title1">{title1}</h2>
             <p className="title2">{title2}</p>
           </div>
@@ -19,7 +23,11 @@ function MainTitleBread({ title1, title2, breadcrumbList = [] }) {
                   <li key={i} className="bread">
                     {/* 如果是最後一個，就只顯示文字，不加箭頭 */}
                     <a href={item.link}>{item.label}</a>
-                    {!isLast && <span className="separator"><HiChevronRight /></span>}
+                    {!isLast && (
+                      <span className="separator">
+                        <HiChevronRight />
+                      </span>
+                    )}
                   </li>
                 );
               })}
@@ -27,7 +35,9 @@ function MainTitleBread({ title1, title2, breadcrumbList = [] }) {
           </nav>
         </header>
         <div className="member-line"></div> {/* 橫線 */}
-        <button onClick={() => back(-1)} className="backBt">&lt; 返回列表</button>
+        <button onClick={() => back(-1)} className="backBt">
+          <FaAngleLeft size={20} /> <span className="backText">返回列表</span>
+        </button>
       </div>
     </>
   );
