42ec9371ec70368c38dda39787ebd92215e36055
amber022834 <sodagreen0228@gmail.com>
2025-06-17 01:02:16 +0800

    加入我們響應式

--- src/fitnavi/Joinus/Joinus.jsx
+++ src/fitnavi/Joinus/Joinus.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import "./Joinus.scss";
 import MainTitle from "../../components/Title/MainTitle";
@@ -6,34 +6,58 @@ import JoinButton from '../../components/joinButton/joinButton';
 
 function Joinus() {
     const navigate = useNavigate();
+
+    // 👉 狀態儲存兩個按鈕的路由
+    const [coachTo, setCoachTo] = useState("/JoinusCarousel");
+    const [nutriTo, setNutriTo] = useState("/NutriJoinusCarousel");
+
+    useEffect(() => {
+        const handleResize = () => {
+            const width = window.innerWidth;
+
+            setCoachTo(width < 1024 ? "/JoinusStep2" : "/JoinusCarousel");
+            setNutriTo(width < 1024 ? "/NutriJoinusStep2" : "/NutriJoinusCarousel");
+        };
+
+        handleResize(); // 初始化設定
+
+        // 若你需要在 resize 後即時更新，可取消註解以下兩行
+        // window.addEventListener("resize", handleResize);
+        // return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
     return (
         <>
             <section id='Joinus-Form'>
-
-                <MainTitle title1="加入我們" title2="成為我們的合作夥伴" className="no-line" />
+                <MainTitle
+                    title1="加入我們"
+                    title2="成為我們的合作夥伴"
+                    className="no-line"
+                />
 
                 <section className='Joinus-introduction'>
                     <p className='BringPro'>把你的專業，帶到更多人眼前。</p>
-                    <p className='BringPro-intro'>歡迎具備專業資格的您加入我們的行列！<br /> 完成表單申請並通過資格審核後， <br />
-                        即可在我們的平台上開課、建立個人專頁，<br /> 讓教學與熱情被看見。</p>
+                    <p className='BringPro-intro'>
+                        歡迎具備專業資格的您加入我們的行列！<br />
+                        完成表單申請並通過資格審核後， <br />
+                        即可在我們的平台上開課、建立個人專頁，<br />
+                        讓教學與熱情被看見。
+                    </p>
                 </section>
 
                 <section className="Joinus-form">
+                    {/* ✅ 教練區塊 */}
                     <div className="imcoach">
-
                         <form action="">
-
                             <div className='coach-label-img'>
                                 <label htmlFor="">
                                     <span className='imcoach-Iam'>我是</span>
                                     <span className='imcoach-coach'>教練</span>
                                 </label>
-
                                 <div className='imcoach-img'>
                                     <img src="./images/Joinus-coach-shadow.png" alt="" />
                                 </div>
                             </div>
-
 
                             <div className='imcoach-youcan'>
                                 <h4>您可以</h4>
@@ -50,36 +74,24 @@ function Joinus() {
                                 <div className='imcoach-youhaveto-line'></div>
                             </div>
                             <p>🤝定期更新可預約時段，追蹤學員運動進度</p>
-                            <p> 🤝撰寫實用訓練文章或影片</p>
+                            <p>🤝撰寫實用訓練文章或影片</p>
                         </form>
 
-                        {/* <button
-                            className='imcoach-joinbutton'
-                            onClick={() => navigate('/JoinusCarousel')}
-                        >
-                            <span>成為合作教練</span>
-                            <img src="/images/Polygon.svg" alt="" />
-                        </button> */}
                         <JoinButton
                             className='joinButton-m only-desktop'
                             text="成為合作教練"
-                            to="/JoinusCarousel"
+                            to={coachTo}
                         />
-
-                        
-
-
                     </div>
 
+                    {/* ✅ 營養師區塊 */}
                     <div className="imnutrionist">
                         <form action="">
-
                             <div className='nutri-label-img'>
                                 <label htmlFor="">
-                                    <span className='imnutrionist-Iam' >我是</span>
+                                    <span className='imnutrionist-Iam'>我是</span>
                                     <span className='imnutrionist-nutrionist'>營養師</span>
                                 </label>
-
                                 <div className='imnutri-img'>
                                     <img src="./images/Joinus-nutri-shadow.png" alt="" />
                                 </div>
@@ -104,22 +116,15 @@ function Joinus() {
                         </form>
 
                         <JoinButton
+                            className='joinButton-m only-desktop'
                             text="成為合作營養師"
-                            to="/NutriJoinusCarousel" />
-                        {/* <button className='imnutrionist-joinbutton' onClick={()=>navigate('/NutriJoinusCarousel')}> */}
-                        {/* <span>成為合作營養師</span>
-                            <img src="/images/Polygon.svg" alt="" /> */}
-
-                        {/* </button> */}
+                            to={nutriTo}
+                        />
                     </div>
-
-
                 </section>
-
             </section>
-
-
         </>
-    )
+    );
 }
-export default Joinus
+
+export default Joinus;
